2ee48f46b7224a9a429a2ab191b2a1861ad0de8a
roudlek <57861799+roudlek@users.noreply.github.com>
2020-11-29 21:53:46 +0100

    Update decompositiodunalgo.py

--- decompositiodunalgo.py
+++ decompositiodunalgo.py
@@ -1,24 +1,33 @@
-print("entrer un nombre entier positif :")
-nbr = int(input())
+while True:
+    print("entrer un nombre entier positif :")
+    nbr = int(input())
 
-Q = nbr // 10
-R = nbr % 10
-print("le chiffre des unites est:", R)
 
-if Q >= 0 :
-    nbr = Q
-    Q = nbr // 10
-    R = nbr % 10
-    print("le chiffre des dizaines est:", R)
 
-    if Q >= 0 :
-        nbr = Q
+    if nbr > 0 :
         Q = nbr // 10
         R = nbr % 10
-        print("le chiffre des centaines est:", R)
+        print("le chiffre des unites est:", R)
 
-        if Q >= 0 :
+        if Q > 0 :
             nbr = Q
             Q = nbr // 10
             R = nbr % 10
-            print("le chiffre des milliers est:", R)
+            print("le chiffre des dizaines est:", R)
+
+            if Q > 0 :
+                nbr = Q
+                Q = nbr // 10
+                R = nbr % 10
+                print("le chiffre des centaines est:", R)
+
+                if Q > 0 :
+                    nbr = Q
+                    Q = nbr // 10
+                    R = nbr % 10
+                    print("le chiffre des milliers est:", R)
+        break
+
+
+    else :
+        print("veuillez entrer un nombre entier positif entre[0;9999]")
