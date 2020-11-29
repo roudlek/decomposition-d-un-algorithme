950a9f4b644a82e1e1249acad790ed7ef01aea61
roudlek <57861799+roudlek@users.noreply.github.com>
2020-11-29 22:48:51 +0100

    Update decompositiodunalgo.py

--- decompositiodunalgo.py
+++ decompositiodunalgo.py
@@ -1,6 +1,6 @@
 while True:      # les phrases qui sont devant le hashtag sont juste des commentaires , while = tanque ca est vrai :
-    print("veuillez entrer un nombre entier positif entre [0;9999] :")
-    nbr = int(input())                  # int pour entrer un nombre entier positif
+    print("veuillez entrer un nombre entier positif entre [0;9999] :")  # ecrire("message")
+    nbr = int(input())                                                  # lire : int pour entrer un nombre entier positif, et input() pour demander a l'utilisateur d'entrer le nombre
 
     if nbr > 0 and (nbr <10000) :       # la on a limiter a 4 chiffre, si nbr support la condition on continue, sinon echec (voir la fin du code)
         Q = nbr // 10                   # Q pour le quotion, // = div dans l'algorithme
