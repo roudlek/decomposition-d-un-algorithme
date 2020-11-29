d6a28781aed87e78b0c61fbd70fb93c2b8ed2e1e
roudlek <57861799+roudlek@users.noreply.github.com>
2020-11-29 22:44:15 +0100

    Update decompositiodunalgo.py

--- decompositiodunalgo.py
+++ decompositiodunalgo.py
@@ -1,10 +1,10 @@
-while True:
-    print("entrer un nombre entier positif :")
-    nbr = int(input())
+while True:      # les phrases qui sont devant le hashtag sont juste des commentaires , while = tanque ca est vrai :
+    print("veuillez entrer un nombre entier positif entre [0;9999] :")
+    nbr = int(input())                  # int pour entrer un nombre entier positif
 
-    if nbr > 0 :
-        Q = nbr // 10
-        R = nbr % 10
+    if nbr > 0 and (nbr <10000) :       # la on a limiter a 4 chiffre, si nbr support la condition on continue, sinon echec (voir la fin du code)
+        Q = nbr // 10                   # Q pour le quotion, // = div dans l'algorithme
+        R = nbr % 10                    # R pour le reste  ,  % = mod dans l'algorithme
         print("le chiffre des unites est:", R)
 
         if Q > 0 :
@@ -24,8 +24,7 @@ while True:
                     Q = nbr // 10
                     R = nbr % 10
                     print("le chiffre des milliers est:", R)
-        break
+        break # ca veut dire stop, et relence le programme si ce cas est choisis
 
     else :
-        print("veuillez entrer un nombre entier positif entre[0;9999]")
-        continue
+        print("echec !")
