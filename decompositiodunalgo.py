984143c114d42fba21d184a834be83075304b4b7
roudlek <57861799+roudlek@users.noreply.github.com>
2020-12-25 18:29:47 +0100

    Update decompositiodunalgo.py

--- decompositiodunalgo.py
+++ decompositiodunalgo.py
@@ -1,15 +1,15 @@
-while True:      # tanque ca est vrai :
+while True:      # tanque ca est vrai :                                                         # Les phrases entre le hashtag sont juste des commentaires !!
     print("veuillez entrer un nombre entier positif entre [1;9999] :")
     nbr = int(input())                  # int pour entrer un nombre entier positif
 
-    if nbr > 0 and nbr <10 :
+    if nbr > 0 and nbr <10 :            # Si
         Q = nbr // 10
         R = nbr % 10
         print("le chiffre des unites est:", R)
         print("Fin de programme")
-        break
+        break                                  # Sortir si la condition est vrai
 
-    elif nbr > 9 and nbr <100 :
+    elif nbr > 9 and nbr <100 :         # Autre si        
         Q = nbr // 10
         R = nbr % 10
         print("le chiffre des unites est:", R)
@@ -18,9 +18,9 @@ while True:      # tanque ca est vrai :
         R = nbr % 10
         print("le chiffre des dizaines est:", R)
         print("Fin de programme")
-        break
+        break                                      # Sortir si la condition est vrai
 
-    elif nbr > 99 and nbr <1000 :
+    elif nbr > 99 and nbr <1000 :               # Autre si
         Q = nbr // 10
         R = nbr % 10
         print("le chiffre des unites est:", R)
@@ -33,9 +33,9 @@ while True:      # tanque ca est vrai :
         R = nbr % 10
         print("le chiffre des centaines est:", R)
         print("Fin de programme")
-        break
+        break                                       # Sortir si la condition est vrai
 
-    elif nbr > 999 and nbr <10000 :
+    elif nbr > 999 and nbr <10000 :                 # Autre si
         Q = nbr // 10
         R = nbr % 10
         print("le chiffre des unites est:", R)
@@ -52,7 +52,7 @@ while True:      # tanque ca est vrai :
         R = nbr % 10
         print("le chiffre des milliers est:", R)
         print("Fin de programme")
-        break
+        break                                           # Sortir si la condition est vrai
 
-    else:
+    else:                                               #sinon
         continue
