7ca67f81034c1f85d155c764481346e8b3cfc7ff
roudlek <57861799+roudlek@users.noreply.github.com>
2020-12-25 18:25:41 +0100

    Update decompositiodunalgo.py

--- decompositiodunalgo.py
+++ decompositiodunalgo.py
@@ -1,31 +1,58 @@
-while True:      # les phrases qui sont devant le hashtag sont juste des commentaires , while = tanque ca est vrai :
-    print("veuillez entrer un nombre entier positif entre [1;9999] :")  # ecrire("message")
-    nbr = int(input())                                                  # lire : int pour entrer un nombre entier positif, et input() pour demander a l'utilisateur d'entrer le nombre
+while True:      # tanque ca est vrai :
+    print("veuillez entrer un nombre entier positif entre [1;9999] :")
+    nbr = int(input())                  # int pour entrer un nombre entier positif
 
-    if nbr > 0 and (nbr <10000) :       # la on a limiter a 4 chiffre, si nbr support la condition on continue, sinon echec (voir la fin du code)
-        Q = nbr // 10                   # Q pour le quotion, // = div dans l'algorithme
-        R = nbr % 10                    # R pour le reste  ,  % = mod dans l'algorithme
+    if nbr > 0 and nbr <10 :
+        Q = nbr // 10
+        R = nbr % 10
         print("le chiffre des unites est:", R)
+        print("Fin de programme")
+        break
 
-        if Q > 0 :
-            nbr = Q
-            Q = nbr // 10
-            R = nbr % 10
-            print("le chiffre des dizaines est:", R)
+    elif nbr > 9 and nbr <100 :
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des unites est:", R)
+        nbr = Q
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des dizaines est:", R)
+        print("Fin de programme")
+        break
 
-            if Q > 0 :
-                nbr = Q
-                Q = nbr // 10
-                R = nbr % 10
-                print("le chiffre des centaines est:", R)
+    elif nbr > 99 and nbr <1000 :
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des unites est:", R)
+        nbr = Q
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des dizaines est:", R)
+        nbr = Q
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des centaines est:", R)
+        print("Fin de programme")
+        break
 
-                if Q > 0 :
-                    nbr = Q
-                    Q = nbr // 10
-                    R = nbr % 10
-                    print("le chiffre des milliers est:", R)
-                    print("Fin de programme")
-        break # ca veut dire stop, et relence le programme si ce cas est choisis
+    elif nbr > 999 and nbr <10000 :
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des unites est:", R)
+        nbr = Q
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des dizaines est:", R)
+        nbr = Q
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des centaines est:", R)
+        nbr = Q
+        Q = nbr // 10
+        R = nbr % 10
+        print("le chiffre des milliers est:", R)
+        print("Fin de programme")
+        break
 
-    else :
-        print("echec !")
+    else:
+        continue
